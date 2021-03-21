41871101c5df49327452ba920f161420be18aa20
Yangxi <fanyangxi@live.cn>
2021-03-21 18:32:47 +0800

    updating first_step, and add unit tests for it

--- erno-cube-solver/src/cfop/cross_solver.rs
+++ erno-cube-solver/src/cfop/cross_solver.rs
@@ -18,43 +18,57 @@ pub mod CrossSolver {
                 ("B".into(), ["B2"].iter().map(|&x| x.into()).collect()),
             ].iter().cloned().collect()),
             ("F".into(), [
-                ("U".into(), ["F", "R", "U'", "R'", "F'"].iter().map(|&x| x.into()).collect()),
-                ("D".into(), ["F'", "R", "U'", "R'"].iter().map(|&x| x.into()).collect()),
-                ("R".into(), ["R", "U", "R'"].iter().map(|&x| x.into()).collect()),
-                ("L".into(), ["L'", "U'", "L"].iter().map(|&x| x.into()).collect()),
+                ("U".into(), ["F", "R", "U\'", "R\'", "F\'"].iter().map(|&x| x.into()).collect()),
+                ("D".into(), ["F\'", "R", "U\'", "R\'"].iter().map(|&x| x.into()).collect()),
+                ("R".into(), ["R", "U", "R\'"].iter().map(|&x| x.into()).collect()),
+                ("L".into(), ["L\'", "U\'", "L"].iter().map(|&x| x.into()).collect()),
             ].iter().cloned().collect()),
             ("B".into(), [
-                ("U".into(), ["B", "L", "U'", "L'", "B'"].iter().map(|&x| x.into()).collect()),
-                ("D".into(), ["B", "R'", "U", "R"].iter().map(|&x| x.into()).collect()),
-                ("R".into(), ["R'", "U", "R"].iter().map(|&x| x.into()).collect()),
-                ("L".into(), ["L", "U'", "L'"].iter().map(|&x| x.into()).collect()),
+                ("U".into(), ["B", "L", "U\'", "L\'", "B\'"].iter().map(|&x| x.into()).collect()),
+                ("D".into(), ["B", "R\'", "U", "R"].iter().map(|&x| x.into()).collect()),
+                ("R".into(), ["R\'", "U", "R"].iter().map(|&x| x.into()).collect()),
+                ("L".into(), ["L", "U\'", "L\'"].iter().map(|&x| x.into()).collect()),
             ].iter().cloned().collect()),
             ("L".into(), [
-                ("U".into(), ["L", "F", "U'", "F'", "L'"].iter().map(|&x| x.into()).collect()),
-                ("D".into(), ["L'", "F", "U'", "F'"].iter().map(|&x| x.into()).collect()),
-                ("F".into(), ["F", "U'", "F'"].iter().map(|&x| x.into()).collect()),
-                ("B".into(), ["B'", "U", "B"].iter().map(|&x| x.into()).collect()),
+                ("U".into(), ["L", "F", "U\'", "F\'", "L\'"].iter().map(|&x| x.into()).collect()),
+                ("D".into(), ["L\'", "F", "U\'", "F\'"].iter().map(|&x| x.into()).collect()),
+                ("F".into(), ["F", "U\'", "F\'"].iter().map(|&x| x.into()).collect()),
+                ("B".into(), ["B\'", "U", "B"].iter().map(|&x| x.into()).collect()),
             ].iter().cloned().collect()),
             ("R".into(), [
-                ("U".into(), ["R'", "F'", "U", "F", "R"].iter().map(|&x| x.into()).collect()),
-                ("D".into(), ["R", "F'", "U", "F"].iter().map(|&x| x.into()).collect()),
-                ("F".into(), ["F'", "U", "F"].iter().map(|&x| x.into()).collect()),
-                ("B".into(), ["B", "U'", "B'"].iter().map(|&x| x.into()).collect()),
+                ("U".into(), ["R\'", "F\'", "U", "F", "R"].iter().map(|&x| x.into()).collect()),
+                ("D".into(), ["R", "F\'", "U", "F"].iter().map(|&x| x.into()).collect()),
+                ("F".into(), ["F\'", "U", "F"].iter().map(|&x| x.into()).collect()),
+                ("B".into(), ["B", "U\'", "B\'"].iter().map(|&x| x.into()).collect()),
             ].iter().cloned().collect()),
         ].iter().cloned().collect();
         return steps;
     }
 
-    pub fn first_step(white_facing: String, color_facing: String) {
+    pub fn first_step(white_facing: &str, color_facing: &str) -> Vec<String> {
         let the_steps = steps();
-        let color_faces = match the_steps.get(&white_facing) {
+        let color_faces = match the_steps.get(white_facing) {
             Some(v) => v,
-            None => panic!("Failed to find in the_steps {}.", &white_facing)
+            None => panic!("Failed to find in the_steps {}.", white_facing)
         };
-        let result = match color_faces.get(&color_facing) {
+        let result = match color_faces.get(color_facing) {
             Some(v) => v,
-            None => panic!("Failed to find in color_faces {}.", &color_facing)
+            None => panic!("Failed to find in color_faces {}.", color_facing)
         };
-        println!("{:#?} days", result);
+        println!(">>> {:?} days", result.to_vec());
+        return result.to_vec();
     }
 }
+
+
+#[cfg(test)]
+mod cross_solver_tests {
+    use crate::cfop::cross_solver::CrossSolver::first_step;
+
+    #[test]
+    fn it_works() {
+        let _results = first_step("F", "U");
+        assert_eq!(vec!["F", "R", "U\'", "R\'", "F\'"], _results);
+    }
+}
+
