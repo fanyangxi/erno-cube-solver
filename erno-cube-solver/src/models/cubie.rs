385367dc7b831ab8fb5aff34c85506b93e96f938
Yangxi-FAN <yxfan@thoughtworks.com>
2021-04-14 20:44:15 +0800

    [cube] adding methods to cubie

--- erno-cube-solver/src/models/cubie.rs
+++ erno-cube-solver/src/models/cubie.rs
@@ -3,6 +3,9 @@ use std::fmt;
 use std::collections::HashMap;
 use crate::models::sticker::Sticker;
 use std::borrow::Borrow;
+use std::collections::hash_map::Keys;
+use std::alloc::Global;
+use std::ops::Index;
 
 // front/back, right/left, up/down.
 // const FACINGS: &str = "FBRLUD";
@@ -29,7 +32,47 @@ impl Cubie {
             facings: results,
         }
     }
+
+    pub fn faces(&self) -> Vec<char> {
+        self.facings.keys().copied().collect::<Vec<_>>()
+    }
+
+    // colors
+    pub fn colors(&self) -> Vec<char> {
+        self.facings.values().map(|f| f.color).copied().collect::<Vec<_>>()
+    }
+
+    pub fn stickers(&self) -> Vec<Sticker> {
+        self.facings.values().copied().collect::<Vec<_>>()
+    }
+
+    pub fn facing_to_color(facing: char) -> char {
+        let result = match COLORS.get(FACINGS.index(facing)) {
+            Some(v) => v,
+            None => panic!("Failed to facing_to_color, facing:{}.", facing)
+        };
+        return *result
+    }
+
+    pub fn color_to_facing(color: char) -> char {
+        let result = match FACINGS.get(COLORS.index(color)) {
+            Some(v) => v,
+            None => panic!("Failed to color_to_facing, color:{}.", color)
+        };
+        return *result
+    }
+
+    pub fn color_facing(self, color: char) -> char {
+        for (key, sticker) in self.facings {
+            if color == sticker.color {
+                return key;
+            }
+        }
+        panic!("Failed to color_facing, color:{}.", color)
+    }
 }
+
+// center: todo...
 
 impl fmt::Display for Cubie {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
